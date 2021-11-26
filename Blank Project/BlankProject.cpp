6ba84e2330e5fd9773d289a9310b1497a9ba2ecd
Taylor Hetherington <taylorhetherington0@gmail.com>
2021-11-26 01:02:19 +0000

    Bigger window by default

--- Blank Project/BlankProject.cpp
+++ Blank Project/BlankProject.cpp
@@ -2,7 +2,7 @@
 #include "Renderer.h"
 
 int main()	{
-	Window w("Make your own project!", 1600, 935, false);
+	Window w("Make your own project!", 1600, 900, false);
 
 	if(!w.HasInitialised()) {
 		return -1;
